2c6cdcd97fcd651ba100e8ee8677f825f2925537
Frederic-Kouassi <frederickouassi.dev.589@gmail.com>
2025-12-13 13:12:52 +0000

    ajout des fichiers de fonction

---
+++ exo3function.py
@@ -0,0 +1,11 @@
+
+def est_pair(n):
+    return n % 2 == 0 
+
+
+ 
+print(est_pair(4))  
+print(est_pair(5))   
+print(est_pair(15)) 
+print(est_pair(28))  
+ 
---
+++ exo3functons.py
@@ -0,0 +1,10 @@
+
+def salutation(prenom, civilite="M."):
+    return f"Bonjour {civilite} {prenom}"
+
+name= input("entrz votre nom svp: ")
+civilite=input("entrez votre civiliter sp:")
+         
+print(salutation(name, civilite))  
+print(salutation(name, civilite))    
+print(salutation(name, civilite))  
